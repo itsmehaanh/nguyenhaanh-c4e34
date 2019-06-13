b2e55115abce07cf4d0df6c22b140c5b9ca47896
Anh Ha Nguyen <itsmehaanh@Anhs-MacBook.local>
2019-06-13 13:39:51 +0700

    Turtle 4

--- Session5/homework5/turtle5_4.py
+++ Session5/homework5/turtle5_4.py
@@ -1,17 +1,17 @@
 import turtle 
 def draw_square(length,color):
-        window = turtle.Screen()
-        t = turtle.Turtle()
-        t.pencolor(color)
+        turtle.color(color)
         for x in range (4):
-            t.forward(length)
-            t.right(90)
+            turtle.forward(length)
+            turtle.right(90)
     
 draw_square(200,"red")
 
 for i in range(30):
-   draw_square(i * 5, 'red')
-   left(17)
-   penup()
-   forward(i * 2)
-   pendown()
+    draw_square(i * 5, 'red')
+    turtle.left(17)
+    turtle.penup()
+    turtle.forward(i * 2)
+    turtle.pendown()
+
+turtle. mainloop()
