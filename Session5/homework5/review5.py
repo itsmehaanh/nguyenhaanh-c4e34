2c3297078c1abda61860567522fa7e15b5215c1a
Anh Ha Nguyen <itsmehaanh@Anhs-MacBook.local>
2019-06-13 09:08:46 +0700

    Review

---
+++ Session5/homework5/review5.py
@@ -0,0 +1,17 @@
+# 1. Why should we use functions at all?
+# - We use functions to bundle a set of instructions that we want to use repeatedly or so complexed that would be better to be self-contained and called when needed.
+# 2. How to define/declare a function?
+# - Function definitions begin with the keyword def. When we define a function, begin with def,followed by the name of the function that we are defining.
+# 3. How to call/ use a function?
+# - when we call a function, we type out its name.
+# 4. What is return, why and how do we use it?
+# - The return enables our function to exit and to give back a value to its caller. It is used when a function is ready to return its value to the caller. To use return, we follow an expression with "returned"
+# 5. Do we have to use return in every function?
+# - No, we only use return for fruitful functions. We don't use return for void functions - those that are not executed for their resulting value.
+# 6. What are function arguments/parameters, why and how do we use them?
+# - Parameter refers to the variable as found in the function definition, while arguments (actual parameters) refers to the input at function call.
+# def NAME (PARAMETERS):
+#     STATEMENTS
+# 7. How to use functions from a different file from a currently working file?
+# - from file import function
+
