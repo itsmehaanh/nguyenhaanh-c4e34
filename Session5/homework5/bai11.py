3fed7ec436ab071ced43cd723dafdd5f7531718a
Anh Ha Nguyen <itsmehaanh@Anhs-MacBook.local>
2019-06-15 12:18:59 +0700

    Bài 11

---
+++ Session5/homework5/bai11.py
@@ -0,0 +1,13 @@
+def is_inside(point, rectangular):             
+    if rectangular[0] <= point[0] <= rectangular[0] + rectangular[2] and rectangular[1] <= point[1] <= rectangular[1] + rectangular[3]:
+        confirm = True
+        print(confirm)
+    else:
+        confirm = False
+        print(confirm)
+
+    return confirm
+
+test = is_inside([100, 120], [140, 60, 100, 200]) 
+
+
